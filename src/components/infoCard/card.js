64281d05c1abdf8bc0afbf2b71d71363f2ea50c2
beniamin iorga <iorgabeni@yahoo.com>
2022-04-28 10:28:02 +0200

    started the migration process for apexcharts
    Changing charts to mixed and pie apexcharts and fixed main and second
    charts css

--- src/components/infoCard/card.js
+++ src/components/infoCard/card.js
@@ -8,8 +8,6 @@ const InfoCard = (props) => {
     const [contentHeader, setContentHeader] = useState([])
     const [contentCO2, setContentCO2] = useState([])
     const [contentCO2CAP, setContentCO2CAP] = useState([])
-    const [CO2, setCO2] = useState(0)
-    const [CO2capita, setCO2capita] = useState(0)
 
     const colorCO2 = ["#7E57C2", "#651FFF", "#3498DB", "#F1C40F", "#E74C3C", "#2ECC71"]
 
@@ -26,29 +24,13 @@ const InfoCard = (props) => {
                         filter: "country,year,co2,coal_co2,gas_co2,oil_co2,cement_co2,flaring_co2,other_industry_co2,co2_per_capita,population"
                     }
                 }
-            ).then(res => {
-                console.log(res.data[0])
-                setContentHeader({
-                    country: res.data[0].country,
-                    year: res.data[0].year,
-                    warning: "Data expressed in tons and popoulation is /100000",
+            )
+                .then(res => {
+                    console.log("printing res from the axios request")
+                    console.log(res)
+                    const data = res.data[0]
                 })
-                setCO2capita(res.data[0].co2_per_capita)
-                setCO2(res.data[0].co2)
-                setContentCO2({
-                    co2: res.data[0].coal_co2,
-                    gas_co2: res.data[0].gas_co2,
-                    oil_co2: res.data[0].oil_co2,
-                    cement_co2: res.data[0].cement_co2,
-                    flaring_co2: res.data[0].flaring_co2,
-                    other_industry_co2: res.data[0].other_industry_co2}
-                )
-                setContentCO2CAP([
-                    {name: "CO2", data: res.data[0].co2},
-                    {name: "Population", data: res.data[0].population / 100000},
-                ])
-            }
-        ).catch(err => {
+            .catch(err => {
                 console.log(err)
             }
         )
@@ -74,13 +56,11 @@ const InfoCard = (props) => {
             <InfoChart
                 dataState={contentCO2}
                 textBar={"CO2 produced:"}
-                dataBar={CO2}
                 colors={colorCO2}
             />
             <InfoChart
                 dataState={contentCO2CAP}
                 textBar={"CO2 per capita:"}
-                dataBar={CO2capita}
                 colors={colorCO2capita}
             />
         </div>
--- src/components/infoCard/infoChart/infoChart.js
+++ src/components/infoCard/infoChart/infoChart.js
@@ -3,67 +3,76 @@ import Chart from "react-apexcharts"
 import "./infoChart.css"
 
 const InfoChart = (props) => {
-    const {dataBar, textBar, colors, dataState} = props;
-    const [CO2, setCO2] = useState([{"name": textBar, "value": dataBar}]);
 
-    console.log("stampo chiavi datastate")
-    console.log(Object.keys(dataState))
-
+    const [CO2, setCO2] = useState([{"name": props.textBar, "value": props.dataBar}]);
+    console.log(props)
+    console.log("stampo chiavi e poi valori datastate")
+    console.log(props.dataState)
     const chartData = {
+        // options: {
+        //     // colors: colors,
+        //     // legend: {
+        //     //     show: true,
+        //     //     // showForNullSeries: true,
+        //     //     // showForZeroSeries: true,
+        //     // },
+        //     // tooltip: {
+        //     //     show: true,
+        //     //     followCursor: true,
+        //     //     fillSeriesColor: true,
+        //     //     // theme: "dark",
+        //     //     // x: {
+        //     //     //     show: true,
+        //     //     // }
+        //     // },
+        //     // chart: {
+        //         // id: "co2_parts",
+        //         // type: "donut"
+        //     // },
+        //     // xaxis: {
+        //     //     categories: ["test", "test1", "test2"], // Object.keys(dataState)
+        //     //     // position: "top",
+        //     //     // tooltip: {
+        //     //     //     enabled: false,
+        //     //     // },
+        //     //     // labels: {
+        //     //     //     show: false,
+        //     //     // }
+        //     // },
+        //     labels: Object.keys(dataState)
+        // },
+        // series: Object.values(dataState)
+
         options: {
-            colors: colors,
-            legend: {
-                show: true,
-                showForNullSeries: true,
-                showForZeroSeries: true,
-            },
-            tooltip: {
-                show: true,
-                followCursor: true,
-                fillSeriesColor: true,
-                theme: "dark",
-                x: {
-                    show: true,
-                }
-            },
-            chart: {
-                id: "co2_parts",
-                type: "donut"
-            },
-            xaxis: {
-                categories: Object.keys(dataState),
-                position: "top",
-                tooltip: {
-                    enabled: false,
-                },
-                labels: {
-                    show: false,
-                }
-            },
+            // labels: ['co2', 'gas_co2', 'oil_co2', 'cement_co2', 'flaring_co2', 'other_industry_co2']
+            labels: Object.keys(props.dataState)
         },
-        series: [
-            {
-                name: "Series 1",
-                data: Object.values(dataState)
-            }
-        ]
+        // series: [22.704, 139.951, 127.438, 7.912, 2.757, 3.055]
+        series: Object.values(props.dataState)
+        // [
+        //     {
+        //         name: "Series 1",
+        //         data: [30, 40, 50]
+        //     }
+        // ]
+    };
+    if (!props.dataState) {
+        return <div/>
     }
-
-return (
-    <div className={"graph-container"}>
-        <Chart
-            height="100%"
-            options={chartData.options}
-            series={chartData.series}
-            type="donut"
-        >
-
-        </Chart>
-        <div className={"bar-container"}>
-            <span>{CO2[0].name} {CO2[0].value}</span>
-
-        </div>
-    </div>
-)
+    else {
+        return (
+            <div className="graph-container">
+                <Chart
+                    height="100%"
+                    options={chartData.options}
+                    series={chartData.series}
+                    type="donut"
+                >
+                </Chart>
+                {/*<div className={"bar-container"}>*/}
+                {/*    <span>{CO2[0].name} {CO2[0].value}</span>*/}
+                {/*</div>*/}
+            </div>
+        )}
 }
 export default InfoChart;
