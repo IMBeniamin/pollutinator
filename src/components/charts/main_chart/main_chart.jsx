741477b95e900aea3f3172d6bc0f2dbf6cb90468
Accardo <balzacca.raffaele@gmail.com>
2022-05-10 11:01:44 +0200

    Refactoring main chart

--- src/components/charts/main_chart/main_chart.jsx
+++ src/components/charts/main_chart/main_chart.jsx
@@ -1,183 +1,173 @@
-import axios from "axios";
 import React, {useEffect, useState} from "react";
-import Chart from 'react-apexcharts'
 import "./main_chart.css";
 import '../../../config';
-import {CircularProgress, Typography} from "@mui/material";
+
+const maxVariance = 0.1
 
 /**
  * MainChart component used for the left chart in the bottom reactive container
  * @returns {JSX.Element} Returns the graphic component.
  */
 const MainChart = (props) => {
+
+    const {data, activeCountry} = props
+
     const [isLoading, setIsLoading] = useState(true);
-    const [chartSettings, setChartSettings] = useState(undefined)
+    const [chartSetting, setChartSetting] = useState(undefined)
+    const [series, setSeries] = useState(undefined)
+
+    // const options = {
+    //     chart: {
+    //         stacked: false,
+    //         animations: {
+    //             enabled: true,
+    //             easing: "easeinout",
+    //             speed: 400,
+    //             animateGradually: {
+    //                 enabled: true,
+    //                 delay: 550
+    //             },
+    //             dynamicAnimation: {
+    //                 enabled: true,
+    //                 speed: 1000
+    //             },
+    //         },
+    //         background: 'transparent',
+    //         toolbar: {
+    //             show: true,
+    //             tools: {
+    //                 download: false,
+    //                 selection: true,
+    //                 zoom: true,
+    //                 zoomin: true,
+    //                 zoomout: true,
+    //                 pan: true,
+    //                 reset: true,
+    //             },
+    //         }
+    //     },
+    //     stroke: {
+    //         width: [2, 2, 2, 2],
+    //         curve: ['smooth', 'smooth', 'smooth', 'straight'],
+    //     },
+    //     plotOptions: {
+    //         bar: {
+    //             dataLabels: {
+    //                 minAngleToShowLabel: 45
+    //             },
+    //             background: 'transparent'
+    //         }
+    //     },
+    //     tooltip: {
+    //         show: true,
+    //         followCursor: true,
+    //         fillSeriesColor: true,
+    //         theme: "dark",
+    //         x: {
+    //             show: true,
+    //         }
+    //
+    //     },
+    //     xaxis: {
+    //         categories: res.data.map(element => element.year),
+    //         position: "bottom",
+    //         labels: {
+    //             show: false,
+    //             style: {
+    //                 colors: ['#FFFFFF']
+    //             }
+    //         },
+    //     },
+    //     yaxis: {
+    //         type: 'numeric',
+    //         labels: {
+    //             style: {
+    //                 colors: ['#FFFFFF']
+    //             }
+    //         }
+    //     },
+    //     legend: {
+    //         show: true,
+    //         labels: {
+    //             colors: ['#FFFFFF']
+    //         }
+    //     },
+    //     theme: {
+    //         mode: 'dark',
+    //         palette: 'palette3'
+    //     },
+    //     fill: {
+    //         opacity: [1, 1, 1, 1],
+    //         type: ["solid", "solid", "solid", "gradient"],
+    //         gradient: {
+    //             inverseColors: false,
+    //             shade: 'light',
+    //             type: "vertical",
+    //             opacityFrom: 0.40,
+    //             opacityTo: 0,
+    //             stops: [0, 100, 100, 100]
+    //         }
+    //     },
+    // }
+
 
     const getData = () => {
-        console.log(props.iso_code)
-        axios
-            .get(global.config.api_url,
-                {
-                    params: {
-                        iso_code: props.iso_code,
-                        year: `1990-2020`,
-                        filter: "year,iso_code,population,gdp,consumption_co2_per_capita,ghg_per_capita"
-                    }
-                })
-            .then(res => {
-                res.data.forEach(element => {
-                    element.gdp_per_capita = (element.gdp / element.population / 1000).toFixed(3);
-                    element.population = (element.population / 1000000).toFixed(3);
-                });
-                setChartSettings({
-                    series: [{
-                        name: "Greenhouse gasses",
-                        type: "line",
-                        data: res.data.map(element => element.ghg_per_capita)
-                    }, {
-                        name: "GDP",
-                        type: "line",
-                        data: res.data.map(element => element.gdp_per_capita)
-                    }, {
-                        name: "CO2",
-                        type: "line",
-                        data: res.data.map(element => element.co2_per_capita)
-                    }, {
-                        name: "Population",
-                        type: "area",
-                        data: res.data.map(element => element.population)
-                    }],
-                    labels: res.data.map(element => element.year),
-                    options: {
-                        chart: {
-                            stacked: false,
-                            animations: {
-                                enabled: true,
-                                easing: "easeinout",
-                                speed: 400,
-                                animateGradually: {
-                                    enabled: true,
-                                    delay: 550
-                                },
-                                dynamicAnimation: {
-                                    enabled: true,
-                                    speed: 1000
-                                },
-                            },
-                            background: 'transparent',
-                            toolbar: {
-                                show: true,
-                                tools: {
-                                    download: false,
-                                    selection: true,
-                                    zoom: true,
-                                    zoomin: true,
-                                    zoomout: true,
-                                    pan: true,
-                                    reset: true,
-                                },
-                            }
-                        },
-                        stroke: {
-                            width: [2, 2, 2, 2],
-                            curve: ['smooth', 'smooth', 'smooth', 'straight'],
-                        },
-                        plotOptions: {
-                            bar: {
-                                dataLabels: {
-                                    minAngleToShowLabel: 45
-                                },
-                                background: 'transparent'
-                            }
-                        },
-                        tooltip: {
-                            show: true,
-                            followCursor: true,
-                            fillSeriesColor: true,
-                            theme: "dark",
-                            x: {
-                                show: true,
-                            }
-                        },
-                        xaxis: {
-                            categories: res.data.map(element => element.year),
-                            position: "bottom",
-                            labels: {
-                                show: false,
-                                style: {
-                                    colors: ['#FFFFFF']
-                                }
-                            },
-                        },
-                        yaxis: {
-                            type: 'numeric',
-                            labels: {
-                                style: {
-                                    colors: ['#FFFFFF']
-                                }
-                            }
-                        },
-                        legend: {
-                            show: true,
-                            labels: {
-                                colors: ['#FFFFFF']
-                            }
-                        },
-                        theme: {
-                            mode: 'dark',
-                            palette: 'palette3'
-                        },
-                        fill: {
-                            opacity: [1, 1, 1, 1],
-                            type: ["solid", "solid", "solid", "gradient"],
-                            gradient: {
-                                inverseColors: false,
-                                shade: 'light',
-                                type: "vertical",
-                                opacityFrom: 0.40,
-                                opacityTo: 0,
-                                stops: [0, 100, 100, 100]
-                            }
-                        },
-                        // title: {
-                        //     text: "Economical influence on pollution per capita",
-                        //     align: 'center',
-                        //     margin: 30,
-                        //     floating: false,
-                        //     style: {
-                        //         fontSize:  '1em',
-                        //         fontWeight:  'bold',
-                        //         color:  'whitesmoke'
-                        //     },
-                        // }
-                    }
-                })
-                setIsLoading(false)
-            })
-            .catch(err => {
-                console.log(err);
-            });
+
+
+        // setChartSettings({
+        //     series: [{
+        //         name: "GDP",
+        //         type: "line",
+        //         data: res.data.map(element => element.gdp_per_capita)
+        //     }, {
+        //         name: "CO2",
+        //         type: "line",
+        //         data: res.data.map(element => element.co2_per_capita)
+        //     }, {
+        //         name: "CO2 per GDP",
+        //         type: "line",
+        //         data: res.data.map(element => element.co2_per_gdp)
+        //     }, {
+        //         name: "Population",
+        //         type: "area",
+        //         data: res.data.map(element => element.population)
+        //     }],
+        //     labels: res.data.map(element => element.year),
+        // })
+
     }
 
     useEffect(() => {
         setIsLoading(true)
-        getData();
+        console.log(props.iso_code)
+
+        let dataActiveCountry = data.filter(obj => obj.iso_code === activeCountry)[0]
+
+        console.log(dataActiveCountry)
+
+        let dataFormatted = {
+            co2_per_gdp: data.filter(obj => Math.abs(obj.co2_per_gdp - dataActiveCountry.co2_per_gdp) <= maxVariance).map(obj => obj.co2_per_gdp),
+            energy_per_gdp: data.filter(obj => Math.abs(obj.energy_per_gdp - dataActiveCountry.energy_per_gdp) <= maxVariance).map(obj => obj.energy_per_gdp)
+        }
+        console.log({dataFormatted})
+        setIsLoading(false)
     }, [props.iso_code]);
 
-
-    return isLoading ? <CircularProgress/> :
-        (
-            <div className="primary-chart">
-                <Typography variant="h6" className="chart-title">
-                    Economical influence on pollution per capita
-                </Typography>
-                <Chart
-                    options={chartSettings.options}
-                    series={chartSettings.series}
-                    height="100%"
-                />
-            </div>
-        );
-};
+    return null
+}
+//     return isLoading ? <CircularProgress/> :
+//         // (
+//         //     // <div className="primary-chart">
+//         //     //     <Typography variant="h6" className="chart-title">
+//         //     //         Economical influence on pollution per capita
+//         //     //     </Typography>
+//         //     //     <Chart
+//         //     //         options={options}
+//         //     //         series={series}
+//         //     //         height="100%"
+//         //     //     />
+//         //     // </div>
+//         // );
+// };
 
 export default MainChart
--- src/components/charts/secondary_chart/secondary_chart.jsx
+++ src/components/charts/secondary_chart/secondary_chart.jsx
@@ -4,6 +4,7 @@ import Chart from "react-apexcharts"
 import {CircularProgress, Typography} from "@mui/material";
 import '../../../config'
 // todo add global share co2 in the title
+
 const label_formatter = {
     share_global_cement_co2: "Cement",
     share_global_coal_co2: "Coal",
@@ -13,121 +14,132 @@ const label_formatter = {
 }
 
 export default function SecondaryChart(props) {
-    const [series, setSeries] = useState(undefined)
+    const [chartSetting, setChartSetting] = useState(undefined)
     const [isLoading, setIsLoading] = useState(true)
-    const chart = {
-        options: {
-            tooltip: {
-                show: true,
-                followCursor: true,
-                fillSeriesColor: true,
-                theme: "dark",
-                x: {
-                    show: true,
-                },
-            },
-            chart: {
-                id: "sharing-global-co2",
-                animations: {
-                    enabled: true,
-                    easing: "easeinout",
-                    speed: 400,
-                    animateGradually: {
-                        enabled: true,
-                        delay: 550
-                    },
-                    dynamicAnimation: {
-                        enabled: true,
-                        speed: 1000
-                    },
-                },
-                background: 'transparent',
-                toolbar: {
-                    show: false,
-                },
-            },
-            theme: {
-                mode: 'dark',
-                palette: 'palette3'
-            },
-            xaxis: {
-                categories: Object.keys(props.data).filter(key => Object.keys(label_formatter).includes(key)).map(key => label_formatter[key]),
-                position: "top",
-                tooltip: {
-                    enabled: false,
-                },
-                labels: {
-                    show: false,
-                }
-            },
-            yaxis: {
-                labels: {
-                    style: {
-                        colors: ['#FFFFFF']
-                    }
-                }
-            },
-            plotOptions: {
-                bar: {
-                    dataLabels: {
-                        minAngleToShowLabel: 45,
-                        size: '35%',
-                        background: 'transparent',
-                    },
-                    distributed: true
-                },
-            },
-            legend: {
-                show: true,
-                labels: {
-                    colors: ['#FFFFFF']
-                }
-            },
-            // title: {
-            //     text: "Global share of CO2 emissions by industry",
-            //     align: 'center',
-            //     margin: 30,
-            //     floating: false,
-            //     style: {
-            //         fontSize:  '1em',
-            //         fontWeight:  'bold',
-            //         color:  'whitesmoke'
-            //     },
-            // }
-        }
-    }
+
+    // title: {
+    //     text: "Global share of CO2 emissions by industry",
+    //     align: 'center',
+    //     margin: 30,
+    //     floating: false,
+    //     style: {
+    //         fontSize:  '1em',
+    //         fontWeight:  'bold',
+    //         color:  'whitesmoke'
+    //     },
+    // }
+
 
     useEffect(() => {
         setIsLoading(true)
 
-        setSeries([{
-                name: "Sharing global CO2 parts",
-                data: Object.keys(label_formatter).reduce((values, key) => {
-                    if (props.data[key] !== undefined)
-                        values.push(props.data[key]);
-                    return values;
-                }, [])
-            }])
+        setChartSetting(
+            {
+                series: [{
+                    name: "Sharing global CO2 parts",
+                    data: Object.keys(label_formatter).reduce((values, key) => {
+                        if (props.data[key] !== undefined)
+                            values.push(props.data[key]);
+                        return values;
+                    }, [])
+                }],
+                options: {
+                    tooltip: {
+                        show: true,
+                        followCursor: true,
+                        fillSeriesColor: true,
+                        theme: "dark",
+                        x: {
+                            show: true,
+                        },
+                    },
+                    chart: {
+                        id: "sharing-global-co2",
+                        animations: {
+                            enabled: true,
+                            easing: "easeinout",
+                            speed: 400,
+                            animateGradually: {
+                                enabled: true,
+                                delay: 550
+                            },
+                            dynamicAnimation: {
+                                enabled: true,
+                                speed: 1000
+                            },
+                        },
+                        background: 'transparent',
+                        toolbar: {
+                            show: false,
+                        },
+                    },
+                    theme: {
+                        mode: 'dark',
+                        palette: 'palette3'
+                    },
+                    xaxis: {
+                        categories: Object.keys(props.data).filter(key => Object.keys(label_formatter).includes(key)).map(key => label_formatter[key]),
+                        position: "top",
+                        tooltip: {
+                            enabled: false,
+                        },
+                        labels: {
+                            show: false,
+                        }
+                    },
+                    yaxis: {
+                        labels: {
+                            style: {
+                                colors: ['#FFFFFF']
+                            }
+                        }
+                    },
+                    plotOptions: {
+                        bar: {
+                            dataLabels: {
+                                minAngleToShowLabel: 45,
+                                size: '35%',
+                                background: 'transparent',
+                            },
+                            distributed: true
+                        },
+                    },
+                    legend: {
+                        show: true,
+                        labels: {
+                            colors: ['#FFFFFF']
+                        }
+                    },
+                    // title: {
+                    //     text: "Global share of CO2 emissions by industry",
+                    //     align: 'center',
+                    //     margin: 30,
+                    //     floating: false,
+                    //     style: {
+                    //         fontSize:  '1em',
+                    //         fontWeight:  'bold',
+                    //         color:  'whitesmoke'
+                    //     },
+                    // }
+                }
 
+            })
         setIsLoading(false)
     }, [props.data]);
 
-    return isLoading ?
-        (
-            <CircularProgress/>
-        )
-        :
+    return isLoading ? <CircularProgress/> :
         (
             <div className="secondary-chart">
                 <Typography variant="h6" className="chart-title">
                     Global share of CO2 emissions by industry
                 </Typography>
                 <Chart
-                    options={chart.options}
-                    series={series}
+                    options={chartSetting.options}
+                    series={chartSetting.series}
                     type="bar"
                     height="100%"
                 />
             </div>
         )
-};
+}
+
